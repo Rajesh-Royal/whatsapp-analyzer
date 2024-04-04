21e8cba1e2268a6e94aee79af4ff6ae2abebba50
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-04 16:50:01 +0530

    util: add unique millisecond timestamp to the message date to make it unique

---
+++ utils/addUniqueTimestamp.ts
@@ -0,0 +1,18 @@
+export const makeTimeStampUnique = (messages: any) => {
+  let lastDate: Date | null = null;
+  let ms = 0;
+
+  return messages.map((message: any) => {
+    const date = new Date(message.date);
+
+    // if the date, month or year has changed, reset the milliseconds
+    if(lastDate && (date.getDate() !== lastDate.getDate() || date.getMonth() !== lastDate.getMonth() || date.getFullYear() !== lastDate.getFullYear())){
+      ms = 0;
+    }
+
+    date.setMilliseconds(++ms);
+    lastDate = date;
+
+    return {...message, date: date.toISOString()}
+  });
+}
