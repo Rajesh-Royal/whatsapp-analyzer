9fbe173814ab627711e20a93e937bd2719883ab7
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-05 20:23:51 +0530

    feature: get message by id api added

---
+++ app/api/get-message-by-id/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/lib/prisma';
+import { logger } from '@/utils/logger';
+
+export async function GET(request: NextRequest) {
+  try {
+    // Process a GET request
+    const urlParams = new URLSearchParams(request.nextUrl.search);
+    const messageId = Number(urlParams.get('messageId'));
+
+    if(!messageId) {
+      return NextResponse.json({ message: 'Message id is required', status: 400, data: [] }, { status: 400 })
+    }
+    
+    const result =  await prisma.message.findFirst({
+      where: {
+        id: messageId
+      }
+    });
+
+    let data: any[] = [];
+    let count = 0;
+    if(result?.id){
+      count = 1;
+      data.push(result);
+    }
+
+    return NextResponse.json({ message: 'Fetched messages successfully', status: 200, data, count });
+  } catch (error: any) {
+    const status = error.status || 500;
+    return NextResponse.json({ message: 'Failed to fetch messages', status, data: [] }, { status })
+  }
+}
