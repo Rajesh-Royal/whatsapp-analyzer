862eab174dd025d3d1e967526e040a8eb9468771
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-06 10:37:12 +0530

    feat: get by author name api enhancement

--- app/api/get-by-author-name/route.ts
+++ app/api/get-by-author-name/route.ts
@@ -5,31 +5,33 @@ export async function GET(request: NextRequest) {
   try {
     const urlParams = new URLSearchParams(request.nextUrl.search);
     const author = urlParams.get('author');
+    const limit = Number(urlParams.get('limit')) || 50;
+    const offset = Number(urlParams.get('offset')) || 0;
 
     if (!author) {
       return NextResponse.json({ message: 'Author name is required', status: 400, data: [] }, { status: 400 })
     }
 
-    const result = await prisma.message.findFirst({
+    const result = await prisma.message.findMany({
       where: {
         author: {
           equals: String(author),
           mode: 'insensitive',
         }
+      },
+      skip: offset,
+      take: limit,
+      orderBy: {
+        date: "asc"
       }
     });
 
-    let data: any[] = [];
-    let count = 0;
-    if (result?.id) {
-      count = 1;
-      data.push(result);
-    } else {
-      return NextResponse.json({ message: 'No message found for this author', status: 204, data, count });
+    if (result.length === 0) {
+      return NextResponse.json({ message: 'No message found for this author', status: 204, data: [], count: 0 });
     }
 
-    return NextResponse.json({ message: 'Fetched message successfully', status: 200, data, count });
-  } catch (error: any) {
+    return NextResponse.json({ message: 'Fetched message successfully', status: 200, data: result, count: result.length });
+  } catch (error: any) {  
     const status = error.status || 500;
     return NextResponse.json({ message: 'Failed to fetch message', status, data: [] }, { status })
   }
