f0eb2dd24e0aeb63ae69b317973f2b3890283045
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-06 12:37:42 +0530

    ref: return undefined if no params

--- lib/utils/getPaginationParams.ts
+++ lib/utils/getPaginationParams.ts
@@ -2,15 +2,15 @@ import { NextRequest } from "next/server";
 
 // Define a type for the pagination parameters
 type PaginationParams = {
-  limit: number;
-  offset: number;
+  limit: number | undefined;
+  offset: number | undefined;
 };
 
 // Create a function to get the pagination parameters from the request
 export function getPaginationParams(request: NextRequest): PaginationParams {
   const urlParams = new URLSearchParams(request.nextUrl.search);
-  const limit = Number(urlParams.get('limit')) || 50; // Default limit is 50
-  const offset = Number(urlParams.get('offset')) || 0; // Default offset is 0
+  const limit = Number(urlParams.get('limit')) || undefined
+  const offset = Number(urlParams.get('offset')) || undefined
 
   return { limit, offset };
 }
