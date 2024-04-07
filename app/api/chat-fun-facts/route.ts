9e775cac9b320aa77b57e5ef1fbe57b8a67516b4
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-07 10:57:36 +0530

    ref: message and count

--- app/api/chat-fun-facts/route.ts
+++ app/api/chat-fun-facts/route.ts
@@ -3,7 +3,7 @@ import prisma from '@/lib/prisma';
 import { createLogger } from '@/lib/logger';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-const logger = createLogger("app/api/get-by-messages-day");
+const logger = createLogger("app/api/chat-fun-facts");
 
 export async function GET(request: NextRequest) {
   try {
@@ -14,10 +14,10 @@ export async function GET(request: NextRequest) {
 
     const result = await getFunFacts(author);
 
-    return NextResponse.json({ message: 'Fetched messages analytics successfully', status: 200, data: [result], count: 0 });
+    return NextResponse.json({ message: 'Fetched messages fun facts successfully', status: 200, data: [result], count: Object.keys(result).length || 0 });
   } catch (error: any) {
     logger.error(error)
-    return NextResponse.json({ message: 'Failed to fetch messages analytics data', status: 500, data: [], errorMessage: (error as PrismaClientKnownRequestError).message }, { status: 500 })
+    return NextResponse.json({ message: 'Failed to fetch messages fun facts data', status: 500, data: [], errorMessage: (error as PrismaClientKnownRequestError).message }, { status: 500 })
   }
 }
 
