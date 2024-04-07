aadcbb574dddc409b340d1ebf7ab7a8511efc17a
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-07 12:02:11 +0530

    API: most active time of day
    desc: get most active time/all of day for an author or for all

---
+++ app/api/active-time-of-day/route.ts
@@ -0,0 +1,82 @@
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/lib/prisma';
+import { createLogger } from '@/lib/logger';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+const logger = createLogger("app/api/active-time-of-day");
+
+export async function GET(request: NextRequest) {
+  try {
+    const urlParams = new URLSearchParams(request.nextUrl.search);
+    const author = urlParams.get('author');
+
+    logger.info('incoming GET request', { params: { author } });
+
+    const result = await getMostActiveTime(author);
+
+    return NextResponse.json({ message: 'Fetched messages fun facts successfully', status: 200, data: [result], count: null });
+  } catch (error: any) {
+    logger.error(error)
+    return NextResponse.json({ message: 'Failed to fetch messages fun facts data', status: 500, data: [], errorMessage: (error as PrismaClientKnownRequestError).message }, { status: 500 })
+  }
+}
+
+interface Author {
+  author: string
+  time: string
+  count: number
+}
+
+const getMostActiveTime = async (author: string | null) => {
+  // Get all messages
+  const messages = await prisma.message.findMany({
+    select: {
+      author: true,
+      date: true,
+    },
+    where: {
+      ...(author ? {author: {
+        equals: author,
+        mode: 'insensitive'
+      }} : {})
+    }
+  });
+
+  // Group messages by author and hour of day
+  const mostActiveTimeByEachAuthorEachHour = messages.reduce((acc, message) => {
+    const hour = message.date.getHours();
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+    const hour12 = hour > 12 ? hour - 12 : (hour === 0 ? 12 : hour); // convert 24-hour format to 12-hour format
+    const time = `${hour12}${amPm}`;
+    const messageAuthor = message.author as string;
+    const key = `${messageAuthor}-${time}`;
+
+    if (!acc[messageAuthor]) {
+      acc[messageAuthor] = [];
+    }
+
+    const existing = acc[messageAuthor].find(item => item.time === time);
+
+    if (existing) {
+      existing.count += 1;
+    } else {
+      acc[messageAuthor].push({
+        author: messageAuthor,
+        time: time,
+        count: 1,
+      });
+    }
+
+    return acc;
+  }, {} as {
+    [author: string]: Author[]
+  });
+
+   // For each author, find the time with the highest message count
+   const mostActiveTimeByEachAuthor: { [author: string]: Author } = {};
+   for (const author in mostActiveTimeByEachAuthorEachHour) {
+     mostActiveTimeByEachAuthor[author] = mostActiveTimeByEachAuthorEachHour[author].sort((a, b) => b.count - a.count)[0];
+   }
+
+  return {mostActiveTimeByEachAuthorEachHour, mostActiveTimeByEachAuthor};
+}
