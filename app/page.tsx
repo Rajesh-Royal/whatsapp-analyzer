298eae5a1bef954bcebeee6655e822342f18a0a6
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-10 09:33:03 +0530

    add: dashboard page and required components and packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
-'use client'
 import FileUpload from "@/components/FileUpload";
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 export default function Index() {
-const session = useSession();
+
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-bold">User Session Next Auth github</h1>
-      <div className="flex flex-col rounded-md bg-neutral-100">
-        <div className="p-4 font-bold rounded-t-md bg-neutral-200 text-black">
-          Current Session
-        </div>
-        <pre className="py-6 px-4 whitespace-pre-wrap break-all text-black">
-          {JSON.stringify(session, null, 2)}
-        </pre>
-      </div>
-    <div>
-    <h1 className="text-3xl font-bold mb-4">Upload whatsapp exported chat file</h1>
-      <FileUpload />
-    </div>
+     Home Page 
+     <LoginButton>
+      <Button>Login</Button>
+     </LoginButton>
     </div>
   )
 }
