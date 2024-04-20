da3f16b9140cf290ff67aec4bd4c18bc3c0bc424
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-20 13:44:40 +0530

    feature: landing page updated

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -2,11 +2,23 @@
 
 import React from 'react'
 import Header from '../dashboardLayout/header'
+import { Button } from '../ui/button'
+import { Heading } from '../common/heading'
 
 const LandingPage = () => {
   return (
     <>
       <Header />
+      <div className='flex h-full w-full d-flex flex-col items-center justify-center'>
+        <Heading
+          title='Welcome to WhatsappAnalyzer'
+          description='Get insights into your chats - Now with more interesting graphs, free statistics and full PDF export, whatsapp chat file upload and more features will be added one by one, please consider to contribute.'
+          className='text-center my-5'
+        />
+        <Button>
+          Go to dashboard
+        </Button>
+      </div>
     </>
   )
 }
