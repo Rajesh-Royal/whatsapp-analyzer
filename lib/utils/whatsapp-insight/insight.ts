8b1c43283cab395ce2f9af303b3ca536b76f2324
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-04-25 08:48:17 +0530

    feature: chat summary calculation

--- lib/utils/whatsapp-insight/insight.ts
+++ lib/utils/whatsapp-insight/insight.ts
@@ -26,7 +26,7 @@ class GetWhatsappChatInsights {
                 wordcloud: this.chatInsightsVars.worddata,
                 timeline: null, // we will implement it later
                 radarMap: null, // we will implement it later
-                summary: null, // we will implement it later
+                summary: this.chatSummary(),
                 basedOnDays: null, // we will implement it later
                 userspecific: this.chatInsightsVars.userdata
             },
@@ -112,6 +112,62 @@ class GetWhatsappChatInsights {
         emojiData.All.emojiStat.totalUniqueEmojis = [...new Set(emojiData.All.emojiUsage.map(emoji => emoji.emoji))].length;
 
         this.chatInsightsVars.emojidata = emojiData;
+    }
+
+    /**
+     * Generate a summary of the whatsapp chat.
+     */
+    chatSummary(): { leastTexts: string; mostTexts: string; noOfLinks: number; totalDays: number; totalMedia: number; totalMessageExchanged: number; totalUsers: number; totalWords: number } {
+        const messageCountByUser: { [username: string]: number } = {};
+        let totalLinks = 0;
+        let totalMedia = 0;
+    
+        // Count messages and links (excluding system messages)
+        this.chatDatabase.forEach(message => {
+            if (message.author !== "system") {
+                const author = message.author as string;
+                messageCountByUser[author] = (messageCountByUser[author] || 0) + 1;
+                if (message.message.includes('http')) {
+                    totalLinks++;
+                }
+                if (message.message.includes('<Media omitted>')) {
+                    totalMedia++;
+                }
+            }
+        });
+    
+        // Find user with least and most texts
+        let leastTextsUser = '';
+        let mostTextsUser = '';
+        let leastTextsCount = Infinity;
+        let mostTextsCount = 0;
+        Object.entries(messageCountByUser).forEach(([user, count]) => {
+            if (count < leastTextsCount) {
+                leastTextsCount = count;
+                leastTextsUser = user;
+            }
+            if (count > mostTextsCount) {
+                mostTextsCount = count;
+                mostTextsUser = user;
+            }
+        });
+    
+        // Calculate total days, total messages, total words, and total users
+        const totalDays = Math.ceil((new Date(this.chatDatabase[this.chatDatabase.length - 1].date).getTime() - new Date(this.chatDatabase[0].date).getTime()) / (1000 * 60 * 60 * 24));
+        const totalMessageExchanged = this.chatDatabase.filter(message => message.author !== "system").length;
+        const totalWords = this.chatInsightsVars.totalwords;
+        const totalUsers = Object.keys(messageCountByUser).length;
+    
+        return {
+            leastTexts: leastTextsUser,
+            mostTexts: mostTextsUser,
+            noOfLinks: totalLinks,
+            totalDays,
+            totalMedia,
+            totalMessageExchanged,
+            totalUsers,
+            totalWords
+        };
     }
 
 }
