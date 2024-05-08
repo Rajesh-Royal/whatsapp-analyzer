c0f6cd0168fa53a766e5c267fa56081335327076
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-05-08 14:16:48 +0530

    feature: opengraph tags addedd

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,43 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import NextThemeProviders from "@/providers/theme-provider";
 import { ChatInsightStoreProvider } from "@/providers/chatInsightStoreProvider";
+import { SITE_NAME } from "@/lib/utils/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const siteTitle = `${SITE_NAME} - WhatsApp Analytics and Insights app`;
 export const metadata: Metadata = {
-  title: "WhatsApp Chat Analyzer",
+  title: siteTitle,
   description:
-    "A full fledged application to analyze your whatsapp chat, fun facts, and its trends",
+    "A full fledged application to analyze your whatsapp chat, fun facts, and its trends. It is 100% secure",
+  themeColor: "#daf8c4",
+  verification: {
+    google: "google-site-verification=CRwqvsQjLJimgYeoQ0xs3jl2b18mfRSrFkci_OTSabE"
+  },
+  openGraph: {
+    type: 'website',
+    title: siteTitle,
+    description: "A full fledged application to analyze your whatsapp chat, fun facts, and its trends. It is 100% secure",
+    images: {
+      height: "<generated>",
+      width: "<generated>",
+      url: '"<generated>"'
+    },
+    url: "https://whatsapp-chat-trends.vercel.app",
+  },
+  twitter: {
+    title: siteTitle,
+    description: "A full fledged application to analyze your whatsapp chat, fun facts, and its trends. It is 100% secure",
+    card: "summary_large_image",
+    site: "https://whatsapp-chat-trends.vercel.app",
+    images: {
+      height: "<generated>",
+      width: "<generated>",
+      url: '"<generated>"'
+    },
+    creator: "Raj_896",
+  }
+
 };
 
 export default async function RootLayout({
--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -1,1 +1,2 @@
 export const ZOD_VALIDATION_ERROR = "ZOD_VALIDATION_ERROR";
+export const SITE_NAME = "TrendyWApp";
