136162d61509318f9dee32ec8669abf45d5480d6
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-05-16 10:06:51 +0530

    ref: hide sidebar in landing page

--- components/dashboardLayout/mobile-sidebar.tsx
+++ components/dashboardLayout/mobile-sidebar.tsx
@@ -5,10 +5,14 @@ import { SideNav } from "@/components/dashboardLayout/side-nav";
 import { NavItems } from "@/components/constants/side-nav";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 export const MobileSidebar = () => {
   const [open, setOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
+
+  const isDashboardPath = usePathname().includes('/dashboard');
 
   useEffect(() => {
     setIsMounted(true);
@@ -19,7 +23,7 @@ export const MobileSidebar = () => {
   }
 
   return (
-    <>
+    isDashboardPath ? <>
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger asChild>
           <div className="flex items-center justify-center gap-4">
@@ -44,6 +48,19 @@ export const MobileSidebar = () => {
           </div>
         </SheetContent>
       </Sheet>
-    </>
+    </> : <div className="flex items-center justify-center gap-4">
+      <Link
+        href={"/"}
+        className="flex items-center justify-between gap-2"
+      >
+        <Image
+          src="/logo.png"
+          width={30}
+          height={30}
+          alt="TrendyWapp website logo"
+        />
+        <h1 className="text-lg font-semibold">WhatsApp Analyzer</h1>
+      </Link>
+    </div>
   );
 };
