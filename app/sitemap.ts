a0ed2ed9f738f93c0984347c825934fc7ae42d70
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-05-31 11:15:54 +0530

    feat: add sitemap and robots.txt to help with search engine

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://whatsapp-chat-trends.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://whatsapp-chat-trends.vercel.app/dashboard',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
