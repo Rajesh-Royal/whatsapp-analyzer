9da0de706ae672ef70b48334e4afc82db2ac0266
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-09-01 10:01:13 +0530

    bugfix: Error: Invariant: headers() expects to have requestAsyncStorage, none available.

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -2,6 +2,4 @@ import { headers } from "next/headers";
 
 export const ZOD_VALIDATION_ERROR = "ZOD_VALIDATION_ERROR";
 export const SITE_NAME = "TrendyWApp";
-export const SITE_BASE_URL =
-  `${headers().get("x-forwarded-proto")}://${headers().get("host")}` ||
-  "https://whatsapp-chat-trends.vercel.app";
+export const SITE_BASE_URL = "https://whatsapp-chat-trends.vercel.app";
